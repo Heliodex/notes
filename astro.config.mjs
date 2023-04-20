ea25ebd8224669cb39b156892d0bd5f950438ade
Lewin Kelly <heli@odex.cf>
2023-04-20 13:48:24 +0000

    Add basic psychology notes at /psychology

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config'
-import mdx from "@astrojs/mdx"
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()]
-})
+  integrations: [mdx(), svelte()]
+});
