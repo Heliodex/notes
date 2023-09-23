fc3ea8477a6c7a1377a04225b9309903ec42034b
Lewin Kelly <heli@odex.cf>
2023-09-23 19:55:09 +0100

    Swap Sass for Stylus and improve other styling

--- src/components/Question.svelte
+++ src/components/Question.svelte
@@ -26,31 +26,31 @@
 	</div>
 {/if}
 
-<style lang="sass">
+<style lang="stylus">
 	button
-		text-align: left
-		max-width: 60ch
-		border: none
-		outline: none
-		margin: 0.2rem
-		font-size: 1rem
-		cursor: pointer
-		color: white
-		background: #274
-		border: 1px solid #132
-		padding: 0.3rem 0.6rem 0.4rem 0.6rem
-		border-radius: 0.5rem
-		transition: all 0.1s ease-in-out
+		text-align left
+		max-width 60ch
+		border none
+		outline none
+		margin 0.2rem
+		font-size 1rem
+		cursor pointer
+		color white
+		background #274
+		border 1px solid #132
+		padding 0.3rem 0.6rem 0.4rem 0.6rem
+		border-radius 0.5rem
+		transition all 0.1s ease-in-out
 		&:hover
-			background-color: #116233
-			border: 1px solid #274
+			background-color #116233
+			border 1px solid #274
 
 	div
-		border: 1px solid #444
-		background: #111
-		border-radius: 0.5rem
-		padding: 0 1rem
-		margin: 0.5rem 0 0.5rem 0.2rem
-		color: #ffaaff
-		width: fit-content
+		border 1px solid #444
+		background #111
+		border-radius 0.5rem
+		padding 0 1rem
+		margin 0.5rem 0 0.5rem 0.2rem
+		color #ffaaff
+		width fit-content
 </style>
